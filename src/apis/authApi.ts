62cbcd1c3dffcb58b519585d259e7dacef1099c1
claire <61533589+joooo24@users.noreply.github.com>
2025-12-26 00:40:05 +0900

    feat: spotify token

---
+++ src/apis/authApi.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { ClientCredentialTokenResponse } from "../models/auth";
+import { ClientID, ClientSecret } from "../configs/authConfig";
+
+const encodedBase64 = (data: string): string => {
+    return Buffer.from(data).toString("base64");
+};
+
+export const getClientCredentialToken = async (): Promise<ClientCredentialTokenResponse> => {
+    try {
+        const body = new URLSearchParams({ grant_type: "client_credentials" });
+
+        const response = await axios.post("https://accounts.spotify.com/api/token", body, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Basic " + encodedBase64(`${ClientID}:${ClientSecret}`),
+            },
+        });
+
+        return response.data.access_token;
+    } catch (error) {
+        throw new Error("Failed to fetch client credential token");
+    }
+};
---
+++ src/configs/authConfig.ts
@@ -0,0 +1,4 @@
+export const ClientSecret = process.env.REACT_APP_SPORTY_SECRET_ID;
+export const ClientID = process.env.REACT_APP_SPORTY_CLIENT_ID;
+export const BaseURI = process.env.REACT_APP_SPORTY_BASE_URI;
+export const RedirectURI = process.env.REACT_APP_SPORTY_REDIRECT_URI;
