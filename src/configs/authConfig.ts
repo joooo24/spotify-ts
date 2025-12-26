110d920dc8d82f47690d9320e3163ca87b07ad27
claire <61533589+joooo24@users.noreply.github.com>
2025-12-27 01:12:17 +0900

    chore: 환경변수 config

--- src/configs/authConfig.ts
+++ src/configs/authConfig.ts
@@ -1,4 +1,3 @@
 export const ClientSecret = process.env.REACT_APP_SPORTY_SECRET_ID;
 export const ClientID = process.env.REACT_APP_SPORTY_CLIENT_ID;
-export const BaseURI = process.env.REACT_APP_SPORTY_BASE_URI;
 export const RedirectURI = process.env.REACT_APP_SPORTY_REDIRECT_URI;
---
+++ src/configs/commonConfig.ts
@@ -0,0 +1,1 @@
+export const SPORTY_BASE_URI = process.env.REACT_APP_SPORTY_BASE_URI;
